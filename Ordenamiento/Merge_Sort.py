6ada3c9418e6d366fda7d4de828cd360923c2dee
SNxRed <sebastian.quintana1@mail.udp.cl>
2025-06-19 15:07:52 -0400

    cambio merge

--- Ordenamiento/Merge_Sort.py
+++ Ordenamiento/Merge_Sort.py
@@ -1,26 +1,35 @@
 def merge_sort(arr):
-    if len(arr) > 1:
-        mid = len(arr)//2
-        L = arr[:mid]
-        R = arr[mid:]
-        merge_sort(L)
-        merge_sort(R)
-        
-        i = j = k = 0
-        while i < len(L) and j < len(R):
-            if L[i] < R[j]:
-                arr[k] = L[i]
-                i += 1
-            else:
-                arr[k] = R[j]
-                j += 1
-            k += 1
-        while i < len(L):
-            arr[k] = L[i]
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    merged_arr = []
+    i = 0  # Pointer for left array
+    j = 0  # Pointer for right array
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            merged_arr.append(left[i])
             i += 1
-            k += 1
-        while j < len(R):
-            arr[k] = R[j]
+        else:
+            merged_arr.append(right[j])
             j += 1
-            k += 1
-    return arr
+
+    # Add remaining elements from left or right arrays (if any)
+    while i < len(left):
+        merged_arr.append(left[i])
+        i += 1
+    while j < len(right):
+        merged_arr.append(right[j])
+        j += 1
+
+    return merged_arr
